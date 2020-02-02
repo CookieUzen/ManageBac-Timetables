087c9d7bd8a92af40f9d75a5de94fad697b8765f
UzenH <uzen.huang@gmail.com>
2020-02-02 23:44:50 +0800

    fix install script bug

--- install.sh
+++ install.sh
@@ -5,6 +5,4 @@ sudo cp timetable-scanner.sh /usr/bin/timetable-scanner
 cd "$HOME"/.config/
 if !([[ -e managebac ]]); then
     mkdir -p managebac
-else
-    echo Managebac folder exsists, edit install.sh to suit your needs.
 fi
